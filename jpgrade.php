896d1f08c01c3af35631ac6afa825abffe45e64d
amy-jacob <77995898+amy-jacob@users.noreply.github.com>
2021-03-29 13:49:36 -0400

    Update jpgrade.php

--- jpgrade.php
+++ jpgrade.php
@@ -1,13 +1,25 @@
 <?php
     
+
+    
     $answer1 = $_POST['question-1-answers'];
-   
+    $answer2 = $_POST['question-2-answers'];
+    $answer3 = $_POST['question-3-answers'];
+    $answer4 = $_POST['question-4-answers'];
+    $answer5 = $_POST['question-5-answers'];
+    $answer6 = $_POST['question-6-answers'];
 
     $totalCorrect = 0;
     
     if ($answer1 == "B") { $totalCorrect++; }
-  
+    if ($answer2 == "A") { $totalCorrect++; }
+    if ($answer3 == "C") { $totalCorrect++; }
+    if ($answer4 == "D") { $totalCorrect++; }
+    if ($answer5 == "B") { $totalCorrect++; }
+    if ($answer6 == "B") { $totalCorrect++; }
     
-    echo "<div id='results'>$totalCorrect / 1 correct</div>";
+    echo "<div id='results'>$totalCorrect / 5 correct</div>";
     
 ?>
+
+ 
